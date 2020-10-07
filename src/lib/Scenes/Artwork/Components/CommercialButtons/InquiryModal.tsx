d248fc7999a150ab44d75099fb04239506c4333a
Joel Rosenblatt <7117670+jo-rs@users.noreply.github.com>
2020-10-07 17:56:16 +0200

    Initial modal and setup

--- src/lib/Scenes/Artwork/Components/CommercialButtons/InquiryModal.tsx
+++ src/lib/Scenes/Artwork/Components/CommercialButtons/InquiryModal.tsx
@@ -2,10 +2,13 @@ import { InquiryModal_artwork } from "__generated__/InquiryModal_artwork.graphql
 import { FancyModal } from "lib/Components/FancyModal/FancyModal"
 import { FancyModalHeader } from "lib/Components/FancyModal/FancyModalHeader"
 import { Text } from "palette"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import NavigatorIOS from "react-native-navigator-ios"
 import { createFragmentContainer, graphql } from "react-relay"
 import { CollapsibleArtworkDetailsFragmentContainer } from "./CollapsibleArtworkDetails"
+
+import { Button, ButtonVariant } from "palette"
+import {ShippingModal} from "./ShippingModal"
 
 interface InquiryModalProps {
   artwork: InquiryModal_artwork
@@ -18,16 +21,26 @@ interface InquiryModalProps {
 
 export const InquiryModal: React.FC<InquiryModalProps> = ({ artwork, ...props }) => {
   const { toggleVisibility, modalIsVisible } = props
+  const [ shippingModalVisibility, setShippingModalVisibility ] = useState(false)
+  const [ location, setLocation ] = useState<any>(null)
 
   return (
     <FancyModal visible={modalIsVisible} onBackgroundPressed={() => toggleVisibility()}>
       <FancyModalHeader leftButtonText="Cancel" onLeftButtonPress={() => toggleVisibility()}>
         Contact Gallery
       </FancyModalHeader>
-      <CollapsibleArtworkDetailsFragmentContainer artwork={artwork} />
+      {/*<CollapsibleArtworkDetailsFragmentContainer artwork={artwork} />*/}
       <Text m={2} variant="title">
         More here
       </Text>
+      <Button onPress={() => setShippingModalVisibility(true)} size="large" block width={100} variant="secondaryOutline">
+        Open Shipping Modal
+      </Button>
+      <ShippingModal
+        toggleVisibility={() => setShippingModalVisibility(!shippingModalVisibility)}
+        modalIsVisible={shippingModalVisibility}
+        setLocation={setLocation}
+      />
     </FancyModal>
   )
 }
