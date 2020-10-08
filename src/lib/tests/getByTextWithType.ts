a53da7dff1d8f671f1b9dcec35192740aa88646b
Pavlos Vinieratos <pvinis@gmail.com>
2020-10-08 12:43:38 +0200

    remove these

--- src/lib/tests/getByTextWithType.ts
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import { ReactTestInstance } from "react-test-renderer"
-
-export const getByTextWithType = <P>(text: string | RegExp, type: React.ComponentType<P>): ReactTestInstance => {
-  return 3
-}
